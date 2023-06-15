9c64bcd867f26356bd8bfe2435d12fe3ed06c5f0
Marcos Pontes <socrampontes@gmail.com>
2023-06-14 22:28:00 -0300

    fluxo de dados template

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,150 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
-import { accounts } from './database'
-import { ACCOUNT_TYPE } from './types'
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { accounts } from "./database";
+import { ACCOUNT_TYPE } from "./types";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.listen(3003, () => {
-    console.log("Servidor rodando na porta 3003")
-})
+  console.log("Servidor rodando na porta 3003");
+});
 
 app.get("/ping", (req: Request, res: Response) => {
-    res.send("Pong!")
-})
+  res.send("Pong!");
+});
 
 app.get("/accounts", (req: Request, res: Response) => {
-    res.send(accounts)
-})
+  res.send(accounts);
+});
 
 app.get("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  try {
+    const id = req.params.id;
+    const result = accounts.find((account) => account.id === id);
 
-    const result = accounts.find((account) => account.id === id) 
+    if (id && id[0] !== "a") {
+      res.statusCode = 400;
+      throw new Error('id invalid , id deve iniciar com a letra "a".');
+    }
+    if (!result) {
+      res.statusCode = 404;
+      throw new Error("Id not entered or not a string");
+    }
 
-    res.status(200).send(result)
-})
+    res.status(200).send(result);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    }
+    res.status(500).send("Erro desconhecido");
+  }
+});
 
 app.delete("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
-
-    const accountIndex = accounts.findIndex((account) => account.id === id)
-
-    if (accountIndex >= 0) {
-        accounts.splice(accountIndex, 1)
+  try {
+    const id = req.params.id;
+    const accountIndex = accounts.findIndex((account) => account.id === id);
+    if (id && id[0] !== "a") {
+      res.statusCode = 400;
+      throw new Error('id invalid , id deve iniciar com a letra "a".');
     }
+    if (accountIndex < 0) {
+      res.statusCode = 404;
+      throw new Error("Id not entered or does not exist");
+    }
+    accounts.splice(accountIndex, 1);
 
-    res.status(200).send("Item deletado com sucesso")
-})
+    res.status(200).send("Item deletado com sucesso");
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.status(500).send("Erro desconhecido");
+    }
+  }
+});
 
 app.put("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  try {
+    const id = req.params.id;
 
-    const newId = req.body.id as string | undefined
-    const newOwnerName = req.body.ownerName as string | undefined
-    const newBalance = req.body.balance as number | undefined
-    const newType = req.body.type as ACCOUNT_TYPE | undefined
+    const newId = req.body.id
+    const newOwnerName = req.body.ownerName 
+    const newBalance = req.body.balance;
+    const newType = req.body.type
+    const account = accounts.find((account) => account.id === id);
 
-    const account = accounts.find((account) => account.id === id) 
-
-    if (account) {
-        account.id = newId || account.id
-        account.ownerName = newOwnerName || account.ownerName
-        account.type = newType || account.type
-
-        account.balance = isNaN(newBalance) ? account.balance : newBalance
+    if(typeof newId !== "string") {
+        res.statusCode = 420
+        throw new Error('id invalid , id deve ser uma string')
     }
 
-    res.status(200).send("Atualização realizada com sucesso")
-})
+    if (newId && newId[0] !== "a") {
+      res.statusCode = 405;
+      throw new Error('id invalid , id deve iniciar com a letra "a".');
+    }
+     
+    if (newId.length < 4){
+      res.statusCode = 403
+      throw new Error('Id deve conter no minimo 4 caracteres')
+    }
+
+    if(typeof newOwnerName !== "string"){
+        res.statusCode = 411;
+        throw new Error("New OwerName deve ser String")
+    } 
+
+
+    if (newOwnerName.length < 3) {
+       
+       res.statusCode = 411;
+       throw new Error("Id deve ter mais de 3 caracteres");
+    } 
+
+   
+
+    if (newBalance !== undefined) {
+      if (typeof newBalance !== "number") {
+        res.statusCode = 422;
+        throw new Error("o valor de balance deve ser numérico");
+      }
+      if (newBalance < 0) {
+        res.statusCode = 400;
+        throw new Error("balance deve ser maior que zero");
+      }
+    }
+
+    if (newType !== undefined) {
+      if (
+        newType !== ACCOUNT_TYPE.BLACK &&
+        newType !== ACCOUNT_TYPE.GOLD &&
+        newType !== ACCOUNT_TYPE.PLATINUM
+      ) {
+        res.statusCode = 400;
+        throw new Error(
+          "O 'type' precisa ser um dos seguintes valores:\nBlack\nOuro\nPlatina"
+        );
+      }
+    }
+
+    if (account) {
+      account.id = newId || account.id;
+      account.ownerName = newOwnerName || account.ownerName;
+      account.type = newType || account.type;
+      account.balance = newBalance || account.balance;
+      //account.balance = isNaN(newBalance) ? account.balance : newBalance;
+      account.balance = newBalance >= 0 ? newBalance : account.balance;
+    }
+
+    res.status(200).send("Atualização realizada com sucesso");
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.status(500).send("Erro desconhecido");
+    }
+  }
+});
